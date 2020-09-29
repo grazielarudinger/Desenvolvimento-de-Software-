e2ba4bac24e3fd2db51d5a479e17e7b14f0673aa
grazielarudinger <72097889+grazielarudinger@users.noreply.github.com>
2020-09-29 18:45:21 -0300

    Add files via upload

---
+++ Atividade 5 Avaliativa/src/restaurante_menu/Vinho.java
@@ -0,0 +1,24 @@
+package restaurante_menu;
+
+public class Vinho {
+
+	public String nome;
+	public double preço;
+
+	public void setNome(String nome) {
+	this.nome = nome;
+	}
+	 
+	public void setPreço(double preço) {
+	this.preço = preço;
+	}
+
+	public double getPreço() {
+	return preço;
+	}
+
+	public String getNome() {
+	return nome;
+	}
+	
+}
